e9ced52d1d574a3cd1bd3aa0dad6de90d4972624
Siddharth Shorya <shoryasiddharth90@gmail.com>
2017-07-01 01:13:25 +0530

    Create take a break mini pro.py

---
+++ take a break mini pro.py
@@ -0,0 +1,9 @@
+# this is a project in whichh we have to code in python
+# program that reminds us to take bearks while  we are working
+import time
+import webbrowser("https://www.youtube.com/watch?v=g55fWNUi9PY")
+print "current time:" + time.ctime()
+brk_count = 5
+for i in brk_count:
+  time.sleep(30)
+  webbrowser.open("http://www.youtube.com/watch?v=dQw4w9WgXcQ")
